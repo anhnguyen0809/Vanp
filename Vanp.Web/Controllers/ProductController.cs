a9d7883ac2e52bd091c0d3a94f9e4e6ec8601b21
truongtphat <tanphat1991996@gmail.com>
2017-06-10 04:18:53 +0700

    Add view products
    show all product

--- Vanp.Web/Controllers/ProductController.cs
+++ Vanp.Web/Controllers/ProductController.cs
@@ -21,6 +21,17 @@ namespace Vanp.Web.Controllers
         [HttpPost]
         public ActionResult Add(Product pro,HttpPostedFileBase productimage)
         {
+            var fileName = Path.GetFileName(productimage.FileName);
+            var duongdan = _context.Products.Select(p => p.Id).FirstOrDefault();
+            var path = Path.Combine(Server.MapPath("~/images/products/"), fileName);
+            if (System.IO.File.Exists(path))
+            {
+                ViewBag.ThongBao = "Hình ảnh đã tồn tại";
+            }
+            else
+            {
+                productimage.SaveAs(path);
+            }
             pro.ModifiedWhen = DateTime.Now;
             pro.CreatedWhen = DateTime.Now;
             pro.CreatedBy = CurrentUser.Id;
@@ -28,6 +39,11 @@ namespace Vanp.Web.Controllers
             pro.ProductImagePath = productimage.FileName;
             _unitOfWork.ProductRepository.Insert(pro);
             _unitOfWork.Save();
+            return View();
+        }
+        public ActionResult ViewListProduct()
+        {
+            _unitOfWork.ProductRepository.GetListbyProducts();
             return View();
         }
     }
