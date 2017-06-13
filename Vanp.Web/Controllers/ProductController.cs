ccdbc1e67cb180b4f12a4fb57e93cb74e70f9b93
truongtphat <tanphat1991996@gmail.com>
2017-06-13 22:28:12 +0700

    update view detail product

--- Vanp.Web/Controllers/ProductController.cs
+++ Vanp.Web/Controllers/ProductController.cs
@@ -14,30 +14,6 @@ namespace Vanp.Web.Controllers
     {
         [AllowAnonymous]
         // GET: Product
-        public ActionResult Add()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Add(Product pro,HttpPostedFileBase productimage)
-        {
-            var fileName = Path.GetFileName(productimage.FileName);
-            var path = Path.Combine(Server.MapPath("~/images/products/"), fileName);
-            if (System.IO.File.Exists(path))
-                ViewBag.ThongBao = "Hình ảnh đã tồn tại";
-            else
-                productimage.SaveAs(path);
-            pro.ModifiedWhen = DateTime.Now;
-            pro.CreatedWhen = DateTime.Now;
-            pro.CreatedBy = CurrentUser.Id;
-            pro.ModifiedBy = CurrentUser.Id;
-            pro.DateFrom = DateTime.Now;
-            pro.DateTo = DateTime.Now.AddDays(7);
-            pro.ProductImagePath = productimage.FileName;
-            _unitOfWork.ProductRepository.Insert(pro);
-            _unitOfWork.Save();
-            return View();
-        }
         public ActionResult ViewListProduct()
         {
             return View(_unitOfWork.ProductRepository.GetListByProduct());
