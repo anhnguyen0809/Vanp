c0ef8db4994d46bcfa8e7d4fe5021836ffa24755
truongtphat <tanphat1991996@gmail.com>
2017-06-13 19:41:01 +0700

    validate insert product

--- Vanp.Web/Areas/Customer/Controllers/CProductController.cs
+++ Vanp.Web/Areas/Customer/Controllers/CProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,16 +12,31 @@ namespace Vanp.Web.Areas.Customer.Controllers
     public class CProductController : AuthController
     {
         // GET: Customer/Product
-        public ActionResult Insert(Product pro)
+        public ActionResult Insert()
         {
-            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Product pro, HttpPostedFileBase productimage)
+        {
             if (CurrentUser.Roles!="Seller")
             {
                 return Redirect("/account/sendrequest");
             }
             else
             {
+                pro.ModifiedWhen = DateTime.Now;
+                pro.CreatedWhen = DateTime.Now;
+                pro.CreatedBy = CurrentUser.Id;
+                pro.ModifiedBy = CurrentUser.Id;
+                pro.DateFrom = DateTime.Now;
+                pro.DateTo = DateTime.Now.AddDays(7);
+                pro.ProductImagePath = productimage.FileName;
+                _unitOfWork.ProductRepository.Insert(pro);
+                _unitOfWork.Save();
                 ViewBag.listCat = _unitOfWork.CategoryRepository.GetListCat();
+                var fileName = Path.GetFileName(productimage.FileName);
+                var path = Path.Combine(Server.MapPath("~/images/products/"), fileName);
             }
             return View();
         }
--- Vanp.Web/Scripts/apps/validate-addpro.js
+++ Vanp.Web/Scripts/apps/validate-addpro.js
@@ -8,30 +8,69 @@
         },
         Price: {
             required: true,
+            number: true
         },
         PriceDefault: {
             required: true,
+            number: true
         },
         ProductDescription: {
             required: true,
         },
+        ProductText: {
+            required: true,
+        },
+        image2:{
+            required: true,
+        },
+        image1:{
+            required: true,
+        },
+        image3: {
+            required: true,
+        }
     },
     messages: {
-        ProductName: "Tên sản phẩm không được bỏ trống.",
+        ProductName: "Tên sản phẩm không được bỏ trống!!!",
         ProductCode: {
             required: "Mã sản phẩm không được để trống!!!",
         },
         PriceDefault: {
             required: "Giá sản phẩm không được để trống!!!",
+            number: "Giá không hợp lệ!!!"
         },
         ProductDescription: {
-            required: "Mô tả không được để trống"
+            required: "Mô tả không được để trống!!!"
         },
         ProductText: {
-            required: "Mô tả không được để trống",
+            required: "Mô tả không được để trống!!!",
         },
+        Price: {
+            required: "Giá mua ngay không được để trống!!!",
+            number: "Giá không hợp lệ!!!"
+        },
+        image2: {
+            required: "Chưa chọn hình thứ 2",
+        },
+        image1: {
+            required: "Chưa chọn hình thứ 1",
+        },
+        image3: {
+            required: "Chưa chọn hình thứ 3",
+        }
     },
 });
-$('.selectpicker').selectpicker({
-    style: 'btn-info',
+tinymce.init({
+    selector: 'textarea',
+    height: 300,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table contextmenu paste code'
+    ],
+    toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    content_css: [
+      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+      '//www.tinymce.com/css/codepen.min.css']
 });
