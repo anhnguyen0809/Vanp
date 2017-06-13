5b293ffb171708e72a9c07873c4a646eb5d7b8f7
anhnguyen0809 <nguyenanh.nnpa@gmail.com>
2017-06-13 13:07:46 +0700

    RequestRepository

---
+++ Vanp.DAL/Repositories/RequestRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vanp.DAL.Entites;
+
+namespace Vanp.DAL
+{
+    public class RequestRepository : GeneralRepository<Request>, IRequestRepository
+    {
+        public RequestRepository(Vanp_Entities context) : base(context)
+        {
+        }
+
+    }
+}
