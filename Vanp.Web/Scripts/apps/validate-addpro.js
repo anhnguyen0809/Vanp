ccb5535633c4dec3554fb757f6f343d53dabeea4
truongtphat <tanphat1991996@gmail.com>
2017-06-13 18:23:06 +0700

    update view insert

--- Vanp.Web/Scripts/apps/validate-addpro.js
+++ Vanp.Web/Scripts/apps/validate-addpro.js
@@ -1,4 +1,4 @@
-﻿$('.register-form').validate({
+﻿$('.insert-form').validate({
     errorElement: 'span', //default input error message container
     errorClass: 'help-block', // default input error message class
     focusInvalid: false, // do not focus the last invalid input
@@ -6,14 +6,11 @@
         ProductName: {
             required: true,
         },
-        ProductCode: {
+        Price: {
             required: true,
         },
         PriceDefault: {
             required: true,
-        },
-        CategoryId: {
-            required: true
         },
         ProductDescription: {
             required: true,
@@ -35,31 +32,6 @@
         },
     },
 });
-$(document).ready(function () {
-    $('.upload').fileinput({
-        showUpload: false,
-    });
-});
-$("input[class='touchspin']").TouchSpin({
-    verticalbuttons: true,
-    max: 1000000000,
-    step: 100000
-});
 $('.selectpicker').selectpicker({
     style: 'btn-info',
-});
-tinymce.init({
-    selector: 'textarea',
-    height: 250,
-    menubar: false,
-    plugins: [
-      'advlist autolink lists link image charmap print preview anchor',
-      'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table contextmenu paste code'
-    ],
-    toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    content_css: [
-      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-      '//www.tinymce.com/css/codepen.min.css'],
-    encoding: "xml",
 });
